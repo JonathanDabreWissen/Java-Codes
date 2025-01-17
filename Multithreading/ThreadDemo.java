cd196d0ee02f3a6238ad0f41a910c6afd73795ce
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-01-17 16:24:07 +0530

    refactor ThreadDemo to use ThreadGroup for better thread management and control

--- Multithreading/ThreadDemo.java
+++ Multithreading/ThreadDemo.java
@@ -41,9 +41,11 @@ class C extends Thread {
 
 public class ThreadDemo {
     public static void main(String[] args) {
-        A a1 = new A();
-        B b1 = new B();
-        C c1 = new C();
+        ThreadGroup tg = new ThreadGroup("Demo");
+        Thread a1 = new Thread(tg, new A());
+        Thread b1 = new Thread(tg, new B());
+        Thread c1 = new Thread(tg, new C());
+        
 
         a1.setName("Rita");
         b1.setName("Geeta");
@@ -54,17 +56,20 @@ public class ThreadDemo {
         c1.start(); 
 
         for(int i = 1; i<=20; i++){
-            System.out.println("Main: " + i);
+            System.out.println("************Main: " + i +"************");
             if(i==5){
-                b1.suspend();
+                tg.suspend();
             }
-            if(i==10){
-                c1.stop();
-            }
-            if(i==12){
+            if(i==8){
                 System.out.println(" a1 is alive: " + a1.isAlive());
                 System.out.println(" b1 is alive: " + b1.isAlive());
                 System.out.println(" c1 is alive: " + c1.isAlive());
+            }
+            if (i==10) {
+                tg.resume();   
+            }
+            if(i==15){
+                tg.stop();
             }
         }
 
