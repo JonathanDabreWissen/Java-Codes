a47b84398f5269590289f56c83f334c6de15883c
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-01-20 14:37:54 +0530

    add JavaLang class with initial structure and comments on core Java concepts

---
+++ JavaLang.java
@@ -0,0 +1,53 @@
+public class JavaLang {
+    
+}
+/*
+ * 
+ * String
+ * StringBuilder
+ * StringBuffer
+ * 
+ * Object
+ * Class 
+ * 
+ * ------------------
+ * Field            |
+ * Method           |---> Part of Reflect class
+ * Constructor      |
+ * -----------------|
+ * 
+ * System
+ * Runtime
+ * Math
+ * Process
+ * Record
+ * 
+ * Thread
+ * ThreadGroup
+ * 
+ * 
+ * Throwable
+ * Exception
+ * Error
+ * RuntimeException
+ * ArithmeticException
+ * NullPointerException
+ * ArrayIndexOutOfBoundsException
+ * IndexOutOfBoundsException
+ * 
+ * 
+ * Number
+ * Integer
+ * Double
+ * Float
+ * Short
+ * Byte
+ * 
+ * 
+ * Cloneable
+ * Runnable
+ * AutoCloseable
+ * Iterable
+ * 
+ * 
+ */
