5b37b654a1b84b741dffd8d117bcd369cfd386c4
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-02-17 18:16:55 +0530

    add examples for Java equality and Stream API operations

---
+++ OneEqualToOne.java
@@ -0,0 +1,18 @@
+public class OneEqualToOne {
+    public static void main(String[] args) {
+        Integer a = 128;
+        Integer b = 128;
+
+        if(a==b){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+        // System.out.println(1==1);
+        // System.out.println(128==128);
+    }
+}
+
+// Read Article: https://freedium.cfd/https://medium.com/@rabinarayandev/why-1-1-is-true-but-128-128-is-false-in-java-4ea544e83eef
