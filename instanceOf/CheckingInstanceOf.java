4a30fccff1f03c02a17b4d05ea3732c5fab511dc
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-02-24 17:38:08 +0530

    add CheckingInstanceOf class to demonstrate instanceof operator usage in Java

---
+++ instanceOf/CheckingInstanceOf.java
@@ -0,0 +1,28 @@
+
+class Animal {
+    void bark(){
+        System.out.println();
+    }
+ }
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Woof!");
+    }
+}
+
+
+public class CheckingInstanceOf {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        
+
+        if (animal instanceof Dog) {
+            Dog dog = (Dog) animal;
+            dog.bark();
+        } else {
+            System.out.println("animal is not a Dog");
+        }
+        
+
+    }   
+}
