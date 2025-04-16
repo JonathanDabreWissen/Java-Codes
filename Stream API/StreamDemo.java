8cfa76f4f6c528876da25f29d9648a8c1694e35e
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-04-16 12:47:07 +0530

    add StreamDemo class to demonstrate stream operations including filtering, sorting, and mapping

---
+++ Stream API/StreamDemo.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+
+
+public class StreamDemo{
+
+	public static void main(String args[]){
+		List<Integer> nums = Arrays.asList(4, 5, 6, 7, 8, 9, 1);
+		
+		//Stream<Integer> data = nums.stream();
+		
+		//long count = data.count();
+		//System.out.println(count);
+
+		//Stream<Integer> sortedData = data.sorted();
+
+
+		//data.forEach(n -> System.out.println(n));
+		//data.forEach(n -> System.out.println(n));
+
+		//sortedData.forEach(n -> System.out.println(n));
+
+		//Stream<Integer> mappedData = data.map(n -> n*2);
+
+		//mappedData.forEach(n -> System.out.println(n));		
+
+
+		//nums.stream().sorted().map(n->n*2).forEach(n -> System.out.println(n));
+		nums.stream()
+			.filter( n -> n%2 == 1) 
+			.sorted()
+			.map(n -> n*2)
+			.forEach( n -> System.out.println(n) );
+
+		
+	}
+}
+
+/*
+
+- Once used stream, it cannot be reused again.
+
+
+
+*/
